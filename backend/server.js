bff59110fa0e646a5df991bdd9fbfd87abcddb3f
hayfa <zaghdoudihayfa@gmail.com>
2025-12-06 15:05:44 -0800

    naamlou control TaskController fih tri, filtrage, recherche et tri par date/deadline

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ app.use(express.json());
 // Connexion MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connecté"))
+<<<<<<< HEAD
   .catch(err => console.error("Erreur MongoDB :", err));
+=======
+  .catch(err => console.error(err));
+>>>>>>> bd98761 (aamlena Configuration du serveur : o zedena des routes d'authentification et de gestion des utilisateurs)
 
 // Import des routes
 const authRoutes = require("./routes/authRoutes");
@@ -22,6 +26,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes); // ✅ AJOUT
 
+<<<<<<< HEAD
 // Route test
 app.get("/", (req, res) => res.send("API Gestion Projets ✔"));
 
@@ -29,3 +34,8 @@ app.get("/", (req, res) => res.send("API Gestion Projets ✔"));
 app.listen(process.env.PORT || 5000, () =>
   console.log("Serveur démarré sur le port " + (process.env.PORT || 5000))
 );
+=======
+app.get("/", (req, res) => res.send("API Gestion Projets ✔"));
+
+app.listen(process.env.PORT || 5000, () => console.log("Serveur démarré"));
+>>>>>>> bd98761 (aamlena Configuration du serveur : o zedena des routes d'authentification et de gestion des utilisateurs)
