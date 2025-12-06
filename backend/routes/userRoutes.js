503be43924f3e0b1de6e381aa0c8fd0b3a7b886d
hayfa <zaghdoudihayfa@gmail.com>
2025-12-05 17:31:59 -0800

    o hedha route GET /users ynajem ystaamlou managers akahaw

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const auth = require("../middlewares/auth");
+const isManager = require("../middlewares/isManager");
 
-// Route test Users
-router.get("/", (req, res) => {
-    res.send("Liste des utilisateurs OK ✔");
+// Route pour obtenir tous les users — manager uniquement
+router.get("/", auth, isManager, async (req, res) => {
+  try {
+    const users = await User.find().select("-motdepasse");
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: "Erreur serveur" });
+  }
 });
 
 module.exports = router;
